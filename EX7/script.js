70ab435cb11a9f2308695b6b5ab343668d68d99b
Patrick-s-Garcia <pg.patrick.gs@gmail.com>
2024-03-20 20:58:49 -0300

    atualização_final

--- EX7/script.js
+++ EX7/script.js
@@ -3,7 +3,7 @@ let maior = -Infinity
 let menor = Infinity
 
 let soma = 0
-let media = soma / 10
+
 
 let positivo = 0
 let negativo = 0
@@ -11,7 +11,7 @@ let negativo = 0
 
 let i = 1
 
-for(i=1;i<10;i++){
+for(i=1;i<=10;i++){
 
     let numero = Number(prompt(`Digite o numero ${i}.`))
 
@@ -34,10 +34,10 @@ for(i=1;i<10;i++){
 
 }
 
+let media = soma / 10
+
 console.log(`A média dos valores é de ${media}`)
 console.log(`A quantidade de valores positivos é ${positivo}`)
 console.log(`A quantidade de valores negativos é ${negativo}`)
 console.log(`O percentual de valores positivos é ${(positivo/10)*100}%`)
 console.log(`O percentual de valores negativos é ${(negativo/10)*100}%`)
-console.log(soma)
-console.log(soma/10)
